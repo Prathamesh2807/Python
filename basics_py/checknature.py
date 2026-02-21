655ec00b817daeb25f825bdcc3688999fa0911cc
Prathamesh2807 <prathamesh.1251070243@vit.edu>
2026-02-21 23:00:05 +0530

    checking positivity of numbers

---
+++ basics_py/checknature.py
@@ -0,0 +1,8 @@
+num = float(input("enter the number : "))
+
+if (num>0):
+    print(f"{num} is positive number ")
+elif (num<0):
+    print(f"{num} is negative number ")
+else:
+    print(f"{num} is zero ")
