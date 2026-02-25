2b15f49dd3853eb099cbe6774fbeddca72a17acb
Prathamesh2807 <prathamesh.1251070243@vit.edu>
2026-02-25 23:05:34 +0530

    compare numbers

---
+++ basics_py/max_min.py
@@ -0,0 +1,9 @@
+a = float(input("enter the number : "))
+b = float(input("enter the number : "))
+
+if (a>b):
+    print(f"{a} is greater than {b}")
+elif(a==b):
+    print(f"both are equal")
+else :
+    print(f"{b} is greater than {a}")
