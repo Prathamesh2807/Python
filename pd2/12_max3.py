e3f45aa408ee61a449c2f732cf6d91c90c37cf69
Prathamesh2807 <prathamesh.1251070243@vit.edu>
2026-03-10 18:30:52 +0530

    max in 3 nums in python

---
+++ pd2/12_max3.py
@@ -0,0 +1,12 @@
+a = int(input("enter the num : "))
+b = int(input("enter the num : "))
+c = int(input("enter the num : "))
+
+if (a>b and b>c):
+    print(f"{a} is the greatest number")
+elif (b>a and a>c):
+    print(f"{b} is the greatest number")
+else :
+    print(f"{c} si the greatest number ")
+
+
