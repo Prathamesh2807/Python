d4bb63ab93fdcf6caa8689a68f9f6221ffe3d992
Prathamesh2807 <prathamesh.1251070243@vit.edu>
2026-03-27 19:31:20 +0530

    list indices

--- lists_tuples/30_listINDEX.py
+++ lists_tuples/30_listINDEX.py
@@ -3,6 +3,4 @@ n  = int(input("enter the no of stu : "))
 for i in range (n):
     name = str(input("enter name  : "))
     var.append(name)
-    print(f"the {i+1} student is : " , var[i],"with index : " , i)
-
-
+    print(f"the {i+1} element is : ", var[i])
