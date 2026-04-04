10772f391b5c1563f422506d52e86c2f87327b4d
Prathamesh2807 <prathamesh.1251070243@vit.edu>
2026-04-04 12:30:32 +0530

    longest frag

--- pd2/17_longestFragment.py
+++ pd2/17_longestFragment.py
@@ -1,2 +1,21 @@
+max_length = 0
+current_length = 0
+previous = None
 
+while True:
+    n = int(input(""))
 
+    if n == 0:
+        break
+
+    if n == previous:
+        current_length += 1
+    else:
+        current_length = 1
+
+    if current_length > max_length:
+        max_length = current_length
+
+    previous = n
+
+print(max_length)
