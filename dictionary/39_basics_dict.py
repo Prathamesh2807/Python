8dbc8d1a2cd88c0aa0593f37014a063671e42994
Prathamesh2807 <prathamesh.1251070243@vit.edu>
2026-04-08 18:36:55 +0530

    basics of dict in oy

---
+++ dictionary/39_basics_dict.py
@@ -0,0 +1,16 @@
+a = {"name" : "prathamesh" , "college" : "vit"}
+
+b = dict(name = "pratham" , college = "iit roorkie")
+
+print(a)
+print(a.get("name"))
+print(b.get("name"))
+
+d = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
+
+# Adding a new key-value pair
+d["age"] = 22
+
+# Updating an existing value
+d[1] = "Python dict"
+print(d)
