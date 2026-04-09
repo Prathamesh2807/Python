43e7c6414b168d646dd0063494475492dd6c39f2
Prathamesh2807 <prathamesh.1251070243@vit.edu>
2026-04-09 19:11:32 +0530

    set function in py

---
+++ dictionary/40_set.py
@@ -0,0 +1,11 @@
+list = [2,3,4]
+
+tuple = ("prathamesh" , 7 , "geeks")
+
+dict = {"name" : "prathamesh" , "exp" : 20}
+
+print(set())
+print(set(list))
+print(set(tuple))
+print(set(dict))
+print(set("prathamesh"))
