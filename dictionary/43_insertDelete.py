d2fec20f8d1c739de35366e39a4e6d43fe481b51
Prathamesh2807 <prathamesh.1251070243@vit.edu>
2026-04-23 22:34:36 +0530

    dictionary key value add

---
+++ dictionary/43_insertDelete.py
@@ -0,0 +1,10 @@
+dict={'brand': 'Ford','model': 'Mustang', 'year': 1964}
+
+#add key and its value to dictionary
+dict["name"] = "prathamesh"
+print(dict)
+
+#delete
+
+del(dict["year"])
+print(dict)
