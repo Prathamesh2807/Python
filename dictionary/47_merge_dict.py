65ca85b78b5ebec5b5c8a473673606d3b4f2c46f
Prathamesh2807 <prathamesh.1251070243@vit.edu>
2026-05-08 22:15:25 +0530

    merge 2 dict

---
+++ dictionary/47_merge_dict.py
@@ -0,0 +1,11 @@
+dict = {"name" : "prathamesh" ,
+        "age" : 18 ,
+        "city" : "sambhajinagar"}
+
+dict2 = {"name" : "amey",
+        "age" : 18 ,
+        "city" : "pune"}
+
+d3 = {**dict,**dict2}
+print(d3)
+
